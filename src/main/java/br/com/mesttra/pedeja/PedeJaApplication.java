bdbecbb0621af347d98706539e2480145ce1cff4
Eduardo <florianoeduardo03@gmail.com>
2022-12-03 02:29:13 -0300

    Commit Inicial

---
+++ src/main/java/br/com/mesttra/pedeja/PedeJaApplication.java
@@ -0,0 +1,13 @@
+package br.com.mesttra.pedeja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedeJaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PedeJaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/mesttra/pedeja/PedeJaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mesttra.pedeja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedeJaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
